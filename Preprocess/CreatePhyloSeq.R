beab94e4348ad683a4e1ffc9a281469c1b45815a
blostein <blostein@umich.edu>
2021-07-12 18:33:29 -0400

    added bar plot for mocks; added plate information to phyloseq objects

--- Preprocess/CreatePhyloSeq.R
+++ Preprocess/CreatePhyloSeq.R
@@ -1,9 +1,4 @@
-# packages
-library(phyloseq)
-library(stringr)
-library(plyr)
-library(dplyr)
-library(stringr)
+
 #Code
 tax<-readRDS(file.path(dada.data, taxname))
 seqtab<-readRDS(file.path(dada.data, seqname))
@@ -20,5 +15,11 @@ dna <- Biostrings::DNAStringSet(taxa_names(phy.asv))
 names(dna) <- taxa_names(phy.asv)
 phy.asv <- merge_phyloseq(phy.asv, dna)
 taxa_names(phy.asv) <- paste0("ASV", seq(ntaxa(phy.asv)))
+#add plate data
+phy.asv@sam_data$Run=ifelse(phy.asv@sam_data$Plate %in% paste('Plate', c(1, 2, 3, 4)), 'Run 1',
+                       ifelse(phy.asv@sam_data$Plate %in% paste('Plate', c(5, 6, 7, 8)), 'Run 2', 
+                        ifelse(phy.asv@sam_data$Plate%in% paste('Plate', c(9, 10, 11, 12)), 'Run 3',
+                                     NA)))
+
 dir.create(file.path(data.out, 'phyloseq'))
 save(phy.asv, file=file.path(data.out, 'phyloseq', "phyasv.Rdata"))
--- Preprocess/FilterSamples.R
+++ Preprocess/FilterSamples.R
@@ -14,7 +14,9 @@ sampVamp<-with(ps@sam_data, xtabs(~SampleType+AmplificationStatus))
 
 #summary of read count in fail vs pass samples 
 readcountsByAmp<-ggplot(data.frame(readcounts=sample_sums(ps), AmplificationStatus=ps@sam_data$AmplificationStatus), aes(fill=AmplificationStatus, x=readcounts))+geom_histogram()+facet_wrap(~AmplificationStatus, ncol=1)+geom_vline(xintercept=readlimit, color='red', linetype=2)+theme_classic()
-readcountsByType<-ggplot(data.frame(readcounts=sample_sums(ps), AmplificationStatus=ps@sam_data$SampleType), aes(fill=AmplificationStatus, x=readcounts))+geom_histogram()+facet_wrap(~AmplificationStatus, ncol=1)+geom_vline(xintercept=readlimit, color='red', linetype=2)+theme_classic()
+readcountsByType<-ggplot(data.frame(readcounts=sample_sums(ps), SampleType=ps@sam_data$SampleType)%>%filter(SampleType!='Empty'), aes(fill=SampleType, x=readcounts))+geom_histogram()+facet_wrap(~SampleType, ncol=1)+geom_vline(xintercept=readlimit, color='red', linetype=2)+theme_classic()
+readcountsbyType_preDecontam<-ggplot(data.frame(readcounts=sample_sums(phy.asv), SampleType=phy.asv@sam_data$SampleType)%>%filter(SampleType!='Empty'), aes(fill=SampleType, x=readcounts))+geom_histogram()+facet_wrap(~SampleType, ncol=1)+geom_vline(xintercept=readlimit, color='red', linetype=2)+theme_classic()
+
   
 weirdFail<-subset_samples(ps, AmplificationStatus=="Fail" & sample_sums(ps)>=readlimit)
 
@@ -25,7 +27,6 @@ morethan1000reads<-subset_samples(ps, sample_sums(ps)>=readlimit)
 #with(morethan1000reads@sam_data, xtabs(~SampleType+AmplificationStatus))
 #passed but shouldn't have 
 EmptyPass<-subset_taxa(subset_samples(ps, sample_sums(ps)>=readlimit & SampleType=="Empty"), taxa_sums(subset_samples(ps, sample_sums(ps)>=1000 & SampleType=="Empty"))>500)
-Extractionkit<-subset_taxa(subset_samples(ps, SampleType=="Extraction"), taxa_sums(subset_samples(ps, SampleType=="Extraction"))>0)
 #plot_bar(EmptyPass, fill="Genus");plot_bar(Extractionkit, fill='Genus')
 
 #final filter
@@ -42,8 +43,22 @@ save(ps_truesamples, file=file.path(data.out, 'phyloseq', "phyasv_truesamples.Rd
 ps@sam_data$ReadLimit=ifelse(sample_sums(ps)>=readlimit, 'pass', 'fail')
 sampleTypeVsReadLimit=ps@sam_data %>% group_by(SampleType, ReadLimit) %>% dplyr::summarise(n=n())
 
+#plot positive controls
+mock_true<-read.csv(file.path(meta.data, "ZymoMockD6305.csv"))
+mock_true$SciName=paste(mock_true$Genus, mock_true$Species)
+mock_true$Name="ZymoBIOMIC D6306"
+ps_mock =transform_sample_counts(subset_taxa(subset_samples(phy.asv, SampleType=="Mock"), taxa_sums(subset_samples(phy.asv, SampleType=="Mock"))>50), function(OTU) OTU/sum(OTU))
+ps_mock_bar=plot_grid(
+  plot_bar(subset_samples(ps_mock, AmplificationStatus=='Pass'), 'Plate', 'Abundance', 'Genus')+facet_wrap(~Run, scales='free_x')+theme_bw()+theme(legend.position = 'none')+theme(axis.text.x = element_text(angle = 45, vjust = .4)), 
+  ggplot(data=mock_true, aes(x=Name, y=X16S.Only/100, fill=SciName))+theme_bw()+geom_bar(position="stack", stat="identity", width=.4, color="black")+scale_x_discrete(name="", labels=c("True mock"))+theme(legend.text = element_text(face="italic"), legend.title=element_blank(), legend.position = "right")+scale_y_continuous(name="")+facet_wrap(~Name)+theme(axis.text.x = element_text(angle = 45, vjust = .4)),
+  align='h', axis = 'tb', rel_widths = c(1, 1))
+
+#plot extraction controls
+Extractionkit<-subset_taxa(subset_samples(ps, SampleType=="Extraction"), taxa_sums(subset_samples(ps, SampleType=="Extraction"))>0)
+
+
 #save
-save(list=c('lessthan1000reads', 'sampleTypeVsReadLimit', 'readcountsByAmp', 'readcountsByType'), file=file.path(output.out, 'Preprocess', paste0('filtersamples_', readlimit, '.Rdata')), compress=T)
+save(list=c('lessthan1000reads', 'sampleTypeVsReadLimit', 'readcountsByAmp', 'readcountsByType', 'readcountsbyType_preDecontam', 'ps_mock_bar'), file=file.path(output.out, 'Preprocess', paste0('filtersamples_', readlimit, '.Rdata')), compress=T)
 
 #print
 print(paste0('Filtering out all samples with total reads of <', readlimit, ' as well as any samples with that many reads but that were not true samples, and samples that were missing all metadata, of an original ', nsamples(ps), ' samples, ', nsamples(ps_truesamples), ' are left', 
